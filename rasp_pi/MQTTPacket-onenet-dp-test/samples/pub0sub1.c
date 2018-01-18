1faa58b05294fcf1a6bd16d6846237aa23370304
rillhu <rillhu@126.com>
2018-01-18 17:10:45 +0800

    add mqtt for onenet dp with paho

---
+++ rasp_pi/MQTTPacket-onenet-dp-test/samples/pub0sub1.c
@@ -0,0 +1,184 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Ian Craggs - initial API and implementation and/or initial documentation
+ *    Sergio R. Caprile - clarifications and/or documentation extension
+ *******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#include "MQTTPacket.h"
+#include "transport.h"
+
+/* This is in order to get an asynchronous signal to stop the sample,
+as the code loops waiting for msgs on the subscribed topic.
+Your actual code will depend on your hw and approach*/
+#include <signal.h>
+
+int toStop = 0;
+
+void cfinish(int sig)
+{
+	signal(SIGINT, NULL);
+	toStop = 1;
+}
+
+void stop_init(void)
+{
+	signal(SIGINT, cfinish);
+	signal(SIGTERM, cfinish);
+}
+/* */
+
+int main(int argc, char *argv[])
+{
+	MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+	int rc = 0;
+	int mysock = 0;
+	unsigned char buf[200];
+	int buflen = sizeof(buf);
+	int msgid = 1;
+	MQTTString topicString = MQTTString_initializer;
+	int req_qos = 0;
+	char payload[128];// = "mypayload";
+	int payloadlen;// = strlen(payload);
+	int len = 0;
+	//char *host = "open.iot.10086.cn";
+	
+	char *host = "183.230.40.39";
+	int port = 6002;
+
+	stop_init();
+	if (argc > 1)
+		host = argv[1];
+
+	if (argc > 2)
+		port = atoi(argv[2]);
+
+	mysock = transport_open(host, port);
+	if(mysock < 0)
+		return mysock;
+
+	printf("Sending to hostname %s port %d\n", host, port);
+
+    data.struct_id[3] = 'T';
+	data.clientID.cstring = "24813778";
+	data.keepAliveInterval = 120;
+	data.cleansession = 0;
+	data.username.cstring = "118217";
+	data.password.cstring = "kwKorRXQBlWebhHuLsD4E48Y7WU=";
+	data.MQTTVersion = 4;
+	
+	len = MQTTSerialize_connect(buf, buflen, &data);
+	rc = transport_sendPacketBuffer(mysock, buf, len);
+
+	/* wait for connack */
+	if (MQTTPacket_read(buf, buflen, transport_getdata) == CONNACK)
+	{
+		unsigned char sessionPresent, connack_rc;
+
+		if (MQTTDeserialize_connack(&sessionPresent, &connack_rc, buf, buflen) != 1 || connack_rc != 0)
+		{
+			printf("Unable to connect, return code %d\n", connack_rc);
+			goto exit;
+		}
+	}
+	else
+		goto exit;
+
+	/* subscribe */
+	topicString.cstring = "mytopic";
+	len = MQTTSerialize_subscribe(buf, buflen, 0, msgid, 1, &topicString, &req_qos);
+
+	rc = transport_sendPacketBuffer(mysock, buf, len);
+	if (MQTTPacket_read(buf, buflen, transport_getdata) == SUBACK) 	/* wait for suback */
+	{
+		unsigned short submsgid;
+		int subcount;
+		int granted_qos;
+
+		rc = MQTTDeserialize_suback(&submsgid, 1, &subcount, &granted_qos, buf, buflen);
+		if (granted_qos != 0)
+		{
+			printf("granted qos != 0, %d\n", granted_qos);
+			goto exit;
+		}
+	}
+	else
+		goto exit;
+
+	/* loop getting msgs on subscribed topic */
+	topicString.cstring = "mytopic";
+	int cnt = 0;
+	while (!toStop)
+	{
+		/* transport_getdata() has a built-in 1 second timeout,
+		your mileage will vary */
+		if (MQTTPacket_read(buf, buflen, transport_getdata) == PUBLISH)
+		{
+			unsigned char dup;
+			int qos;
+			unsigned char retained;
+			unsigned short msgid;
+			int payloadlen_in;
+			unsigned char* payload_in;
+			int rc;
+			MQTTString receivedTopic;
+
+			rc = MQTTDeserialize_publish(&dup, &qos, &retained, &msgid, &receivedTopic,
+					&payload_in, &payloadlen_in, buf, buflen);
+			printf("message arrived %.*s\n", payloadlen_in, payload_in);
+		}
+#if 0
+		printf("publishing sending\n");
+		sprintf(payload,"payload %d",cnt++);
+		payload[19] = '\0';
+		payloadlen = strlen(payload);
+		len = MQTTSerialize_publish(buf, buflen, 0, 0, 0, 0, topicString, (unsigned char*)payload, payloadlen);
+		rc = transport_sendPacketBuffer(mysock, buf, len);
+#endif
+		printf("publishing sending dp\n");
+        topicString.cstring = "$dp";
+
+        
+        memset(payload,0,128);
+		sprintf(payload,"{\"Temperature\":\"%d\", \"Humidity\":\"%d\"}",25,27);
+        printf("payload: %s\n",payload);
+
+        unsigned char *data = (char *)malloc(strlen(payload)+3);
+        data[0] = 3; //data type
+        data[1] = 0x00;
+        data[2] = strlen(payload);
+        memcpy(&data[3],payload,strlen(payload));
+        //mqtt_publish(client, "$dp", data, strlen(out)+3, 0, 0);
+		len = MQTTSerialize_publish(buf, buflen, 0, 0, 0, 0, topicString, (unsigned char*)data, strlen(payload)+3);
+		rc = transport_sendPacketBuffer(mysock, buf, len);
+        free(data);
+        data = NULL;
+        usleep(100000);
+        
+	}
+
+	printf("disconnecting\n");
+	len = MQTTSerialize_disconnect(buf, buflen);
+	rc = transport_sendPacketBuffer(mysock, buf, len);
+
+exit:
+	transport_close(mysock);
+
+	return 0;
+}
